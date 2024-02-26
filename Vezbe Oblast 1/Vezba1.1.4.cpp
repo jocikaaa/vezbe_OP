b2e8f8b99825fffacdd3927162d1fa533478c08e
Jocika <53317873+jocikaaa@users.noreply.github.com>
2024-02-26 18:32:27 +0100

    Add files via upload

---
+++ Vezbe Oblast 1/Vezba1.1.4.cpp
@@ -0,0 +1,21 @@
+/* Zbirka strana 8, zadatak 4*/
+
+#include <stdio.h>
+
+// POD A, jedan poziv funkcije, stampanje teksta u vise linija
+
+main()
+{
+	printf("\nDobrodosao/la u\n\n jezik C!\n\n"); // Stampa tekst u dva reda.
+}
+
+// POD B, vise poziva funkcije, stampanje teksta u jednom redu
+
+/*main()
+{
+	printf("\nDobrodosao/la u");
+	printf(" Jezik C!\n\n");
+}*/
+
+// POD C, je potrebno da program sadrzi komentare u posebnoj linij i u produzetku naredbe. Na liniji 9 je komentar u produzetku naredbe.
+// Ukoliko testirate program, sklonite komentar sa dela POD B, a komentarisite ceo deo POD A.
