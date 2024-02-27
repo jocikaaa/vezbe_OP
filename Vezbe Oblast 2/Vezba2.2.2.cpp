4d7e0318785cc25622f89a0a03b1740ded3ca41d
Jocika <53317873+jocikaaa@users.noreply.github.com>
2024-02-27 19:02:36 +0100

    Add files via upload

---
+++ Vezbe Oblast 2/Vezba2.2.2.cpp
@@ -0,0 +1,20 @@
+/* Zbirka strana 21, zadatak 1*/
+
+#include <stdio.h>
+
+
+
+int main()
+{
+	int x;
+
+	printf("\nUnesite ceo broj: ");
+	scanf("%d", &x);
+
+	printf("\n%-10d", x);
+	printf("\n%010d", x);
+	printf("\n%10d\n\n", x);
+}
+
+/*Ukoliko se trazi poravnanje uz neku od margina, a pritom se ispred broja MORAJU NALAZITI NULE,
+samo treba ispred sirine polja dodati 0, i to je to.
