1b41b614d1a97e7b6820da197c27a8030f8a2605
Jocika <53317873+jocikaaa@users.noreply.github.com>
2024-02-27 18:37:06 +0100

    Add files via upload

---
+++ Vezbe Oblast 2/Vezba2.1.4.cpp
@@ -0,0 +1,19 @@
+/* Zbirka strana 20, zadatak 5*/
+
+#include <stdio.h>
+
+
+
+int main()
+{
+	int a = 555;
+
+	//POD A
+	printf("/%10d/\n", a);
+	
+	//POD B
+	printf("/%-10d/\n\n", a);
+}
+
+//Zadatak se nadovezuje na vezbu 2.1.2, i nije nista komplikovan. Koristi se samo formatiranje sa sirinom polja.
+
