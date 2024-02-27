70874b75697594b366c42d72812aa6081af9bd8b
Jocika <53317873+jocikaaa@users.noreply.github.com>
2024-02-27 18:31:25 +0100

    Add files via upload

---
+++ Vezbe Oblast 2/Vezba2.1.3.cpp
@@ -0,0 +1,19 @@
+/* Zbirka strana 19, zadatak 3*/
+
+#include <stdio.h>
+
+
+
+main()
+{
+	int a, b, c; //tri promenljive. Posto su istog tipa mozemo ih deklarisati u istom redu.
+
+	printf("\nUnesite brojeve a, b i c: ");
+	scanf("%d%d%d", &a, &b, &c); //Tri unosa, tako da moramo 3 puta napisati %d
+
+	printf("\nBroj a je: %d\nBroj b je: %d\nBroj c je: %d\n\n", a, b, c);
+
+}
+
+
+
