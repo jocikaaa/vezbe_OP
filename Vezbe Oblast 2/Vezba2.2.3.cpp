95a4d2bbe09697c1e0f73528fbdc93963fa2b2a1
Jocika <53317873+jocikaaa@users.noreply.github.com>
2024-02-28 15:20:05 +0100

    Add files via upload

---
+++ Vezbe Oblast 2/Vezba2.2.3.cpp
@@ -0,0 +1,20 @@
+/* Zbirka strana 21, zadatak 3*/
+
+#include <stdio.h>
+
+
+
+int main()
+{
+	double x;
+
+	printf("\nUnesite realan broj: ");
+	scanf("%lf", &x);
+
+	printf("\n%.0f", x);
+	printf("\n%-10.4f", x);
+	printf("\n%10.2f\n\n", x);
+	
+}
+
+
