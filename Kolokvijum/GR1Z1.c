61ecf366b32c2953a5dd47c6b26be0907f305bfd
Jocika <53317873+jocikaaa@users.noreply.github.com>
2024-04-12 22:36:49 +0200

    Add files via upload

---
+++ Kolokvijum/GR1Z1.c
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+#define N 32
+#define MASKA 0x0f
+
+int main()
+{	
+	
+	int x,y;
+	int pojY, modX, cifreX; // pojavljivanje Y unutar X, X za deljenje i broj Cifara X
+	int bin;
+	int i;
+
+	printf("Unesite broj x i y (Oba pozitivna, y u opsegu od 0 do 9)");
+	scanf("%d%d", &x, &y);
+	
+	printf("\nDecimalni oblik - X:%d, Y:%d\n", x, y);
+	printf("Heksadecimalni oblik - X:%X, Y:%X\n", x, y);
+
+	if(y >= 0 && y <= 9)
+	{
+		modX = x;
+		while(modX)
+		{
+			if(modX%10 == y)
+			{
+				pojY ++;
+			}
+			cifreX++;
+			modX /= 10;
+		}
+
+		modX = x;
+		for(i = 0; i < N; i++)
+		{
+			if((modX & MASKA) == y)
+			{
+				bin++;
+			}
+			modX /= 2;
+		}
+		printf("\nX ima %d Cifara", cifreX);
+		printf("\nCifra %d, se moze naci %d puta unutar X", y, pojY);
+		printf("\n%d Puta mozemo naci binarni oblik Y unutar X", bin);
+	}
+	else
+	{
+		printf("Y je van opsega.");
+	}
+	
+	printf("\n\nProgram Zavrsen.\n");
+}
---
+++ Kolokvijum/GR1Z2.c
@@ -0,0 +1,55 @@
+﻿#include <stdio.h>
+
+#define MIN 5
+#define MAX 15
+
+main() {
+    int i, j, k, n, znak, slovo, praznih;
+
+    do {
+        printf("\nZnak: ");
+        znak = getchar();
+        getchar();
+    }while(znak != '*' && znak != '+');
+
+    do {
+        printf("\nNeparan Broj n (u posegu od %d do %d): ", MIN, MAX);
+        scanf("%d", &n);
+    }while(n < MIN || n > MAX || !(n % 2));
+
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= n; j++) {
+            putchar(znak);
+        }
+        putchar('\n');
+    }
+    putchar('\n');
+
+    for(i = 1; i <= (n + 2); i++) {
+        putchar(znak);
+    }
+    putchar('\n');
+
+    putchar(znak);
+    for(slovo = 'a', j = 1; slovo <= 'z'; slovo++, j++) {
+        putchar(slovo);
+        if(slovo == 'z') {
+            for(k = j; k < n; k++, j++) {
+                putchar(' ');
+            }
+        }
+        if(j == n) {
+            j = 0;
+            putchar(znak);
+            putchar('\n');
+            putchar(znak);
+        }
+    }
+
+    for(i = 2; i <= (n + 2); i++) {
+        putchar(znak);
+    }
+    putchar('\n');
+
+}
+
